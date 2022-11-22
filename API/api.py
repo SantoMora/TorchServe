07b8e1b492e38b4b51144d87fe93c9b60f6c2e17
Mauricio Valdovinos <mauricio.valdovinos@softtek.com>
2022-11-22 07:34:22 +0000

    modified api

--- API/api.py
+++ API/api.py
@@ -6,6 +6,24 @@ logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 
+# Print out bucket names
+def listBuckets(bucketName):
+    s3 = boto3.resource("s3")   
+    for bucket in s3.buckets.all():
+        if bucket.name == bucketName:
+            print(bucket.name)
+            return True
+    return False
+
+def uploadFile(bucketName, fileName):
+    s3 = boto3.client("s3")      
+    if listBuckets(bucketName):
+        s3.upload_file(
+            Filename=fileName,
+            Bucket=bucketName,
+            Key=fileName,
+        )
+
 @app.route('/', methods=['GET'])
 def home():
   return "Model Serving API v0.0.1", 200
@@ -14,7 +32,6 @@ def home():
 def saveMarToS3():
   try:
     req = request.get_json()
-    alert = Outliers(req['params'], req['profile_name'], req['data'])
     if alert['completed']:
       return jsonify(alert['msn']), 200
     else:
@@ -22,3 +39,10 @@ def saveMarToS3():
   except Exception as e:
     return str(e), 400
 
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port='5000',
+        debug=True,
+        use_reloader=False
+    )

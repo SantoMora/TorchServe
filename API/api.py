2236c7e922bca7ea130bbcb199f6fd52e80a0642
Ubuntu <ubuntu@ip-172-31-95-73.ec2.internal>
2022-11-23 11:10:33 +0000

    added pull file from s3 endpoint

--- API/api.py
+++ API/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_file
 import boto3
 import logging
 
@@ -26,6 +26,16 @@ def uploadFile(bucketName, fileName):
         return True
     return False
 
+def downloadFile(bucketName, fileName):
+  try:
+      s3 = boto3.resource("s3")   
+      s3.Bucket(bucketName).download_file(fileName, fileName.split('/')[1])
+      return True
+  except botocore.exceptions.ClientError as e:
+      if e.response['Error']['Code'] == "404":
+          #print("The object does not exist.")
+          return False
+
 @app.route('/', methods=['GET'])
 def home():
   return "Model Serving API v0.0.1", 200
@@ -34,9 +44,20 @@ def home():
 def saveMarToS3():
   try:
     req = request.get_json()
-    fileUploaded = uploadFile(req['bucketName'], req['modelName'])
+    fileDownloaded = uploadFile(req['bucketName'], req['modelName'])
     if fileUploaded:
       return jsonify("Completed"), 200
+    else:
+      return jsonify("Failed"), 400
+  except Exception as e:
+    return str(e), 400
+
+@app.route('/get/mar', methods=['GET'])
+def getMarFromS3():
+  try:
+    fileDownloaded = downloadFile(request.headers['bucketName'], request.headers['fileName'])
+    if fileDownloaded:
+      return send_file(request.headers['fileName'].split('/')[1]), 200
     else:
       return jsonify("Failed"), 400
   except Exception as e:
